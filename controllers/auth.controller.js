b00d615cd12726eeefeaa9f78d271eecc752e9d3
Duy Phung <duypv98@gmail.com>
2020-03-17 00:33:50 +0700

    add comment

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -6,22 +6,30 @@ const { JWT_KEY, SALT } = process.env;
 
 const register = async (req, res) => {
   const { username, email, password } = req.body;
+  // Check if empty request body
+  if (!req.body || !username || !password || !email) return res.status(400).json({ error: { message: 'Bad Request: empty data' } });
+  // Check if username or email has been taken
   const exUser = await User.findOne().or([{ username }, { email }]);
   if (exUser) return res.status(409).json({ error: { message: 'Username or email exists' } });
+  // Create new user
   const newUser = new User();
   newUser.username = username;
   newUser.email = email;
-  newUser.password = crypto.pbkdf2Sync(password, SALT, 1000, 64, 'sha512').toString('hex');
+  newUser.password = crypto.pbkdf2Sync(password, SALT, 1000, 64, 'sha512').toString('hex'); // hashing password
   await newUser.save();
   return res.status(200).json({ data: { message: 'Success' } });
 };
 
 const login = async (req, res) => {
   const { username, password } = req.body;
+  // Check if empty request body
+  if (!req.body || !username || !password) return res.status(400).json({ error: { message: 'Bad Request: empty data' } });
+  // Check login info
   const user = await User.findOne({ username });
   if (!user) return res.status(401).json({ error: { message: 'Invalid Username' } });
   const isValidPassword = crypto.pbkdf2Sync(password, SALT, 1000, 64, 'sha512').toString('hex') === user.password;
   if (!isValidPassword) return res.status(401).json({ error: { message: 'Invalid Password' } });
+  // Sign JWT and send it to Client
   const token = jwt.sign({ uid: user.get('_id') }, JWT_KEY, { expiresIn: '5m' });
   return res.status(200).json({ data: { token } });
 };
--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -5,7 +5,9 @@ const { JWT_KEY } = process.env;
 module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader) return res.status(401).json({ error: { message: 'Unauthorized' } });
+  // Get token from request authorization header: (Authorization: Bearer <token>)
   const token = authHeader.split(' ')[1];
+  // Verify token and get uid - user_id from token
   jwt.verify(token, JWT_KEY, (err, payload) => {
     if (err) res.status(403).json({ error: { message: 'Token Expired or Invalid Token' } });
     req.uid = payload.uid;
