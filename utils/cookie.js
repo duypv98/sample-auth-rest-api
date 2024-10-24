668f707b66ca025479d23a32682b4f670b0f4e0b
duypv <duypv98@gmail.com>
2024-10-24 09:48:13 +0700

    fix: cookie options

--- utils/cookie.js
+++ utils/cookie.js
@@ -6,7 +6,8 @@ const { ParseEnv } = require("./env");
  * }} args
  */
 function getCookieOptions(args = {}) {
-  const cookieOptions = { httpOnly: true, maxAge: args.maxAge };
+  const { maxAge = null } = args;
+  const cookieOptions = { httpOnly: true, maxAge };
   if (ParseEnv.bool('COOKIE_SECURED_ENDPOINT')) cookieOptions.secure = true;
   if (process.env.COOKIE_SAME_SITE) cookieOptions.sameSite = process.env.COOKIE_SAME_SITE;
   if (process.env.COOKIE_DOMAIN) cookieOptions.domain = process.env.COOKIE_DOMAIN;
